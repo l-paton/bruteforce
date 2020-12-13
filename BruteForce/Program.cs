b4017a763ebe9ad613aad9a38c43db103e71a8fb
L-Paton <63353084+L-Paton@users.noreply.github.com>
2020-12-13 19:27:51 +0100

    first commit

---
+++ BruteForce/Program.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+
+namespace BruteForce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Parser.Default.ParseArguments<Options>(args).MapResult((Options opts) => Options.Attack(opts), errs => 1);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            
+        }
+    }
+}
